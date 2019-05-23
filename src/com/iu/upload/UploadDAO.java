9c3c2647a05bdfea4e714277da8a7965ddb0aa7b
min1744 <dkqtk@naver.com>
2019-05-23 09:27:46 +0900

    Update UploadDAO.java

--- src/com/iu/upload/UploadDAO.java
+++ src/com/iu/upload/UploadDAO.java
@@ -1,5 +1,48 @@
 package com.iu.upload;
 
-public class UploadDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import com.iu.util.DBConnector;
+
+public class UploadDAO {
+	
+	//select
+	public UploadDTO selectOne(int num) throws Exception{
+		UploadDTO uploadDTO = null;
+		Connection con = DBConnector.getConnect();
+		String sql = "select * from upload where no=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, num);
+		ResultSet rs = st.executeQuery();
+		if(rs.next()) {
+			uploadDTO = new UploadDTO();
+			uploadDTO.setPnum(rs.getInt("pnum"));
+			uploadDTO.setNo(rs.getInt("no"));
+			uploadDTO.setoName(rs.getString("oname"));
+			uploadDTO.setfName(rs.getString("fname"));
+		}
+		DBConnector.disConnect(con, st, rs);
+		return uploadDTO;
+	}
+	
+	//update
+	
+	//delete
+	
+	//insert
+	public int insert(UploadDTO uploadDTO) throws Exception{
+		int result=0;
+		Connection con = DBConnector.getConnect();
+		String sql = "insert into upload values(num_seq.nextval, ?, ?, ?)";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, uploadDTO.getNo());
+		st.setString(2, uploadDTO.getoName());
+		st.setString(3, uploadDTO.getfName());
+		result = st.executeUpdate();
+		DBConnector.disConnect(con, st);
+		return result;
+	}
+	
 }
